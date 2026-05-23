25b453689e7a6e739ae1a7b9ad0961c9d85be8ff
monicaortiz20 <ortiznavajomonica@gmail.com>
2026-05-23 22:08:20 +0200

    added more css

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -8,14 +8,14 @@ export default function Tabs({ activeTab, setActiveTab }: TabsProps) {
     <div className="flex w-full items-center gap-3 md:w-fit">
       <button
         onClick={() => setActiveTab("episodes")}
-        className={`rounded-full px-5 py-3 text-sm font-semibold tracking-wide transition-all duration-300 md:px-8 ${activeTab == "episodes" ? `bg-[var(--neon--green-soft)] text-[var(--neon-green)] shadow-[0_0_20px_rgba(163, 255, 18, 0.12)]` : `bg-transparent text-zinc-500 hover:text-white`}`}
+        className={`rounded-full px-6 py-3 text-[20px] font-semibold tracking-wide transition-all duration-300 md:px-8 md:py-4 cursor-pointer ${activeTab == "episodes" ? `bg-[var(--neon--green-soft)] text-[var(--neon-green)] shadow-[0_0_20px_rgba(163, 255, 18, 0.12)]` : `bg-transparent text-zinc-500 hover:text-white`}`}
         style={{ fontFamily: "Orbitron, sans-serif" }}
       >
         Episodes
       </button>
       <button
         onClick={() => setActiveTab("locations")}
-        className={`rounded-full px-5 py-3 text-sm tracking-wide font-semibold transition-all duration-300 md:px-8 ${activeTab == "locations" ? `bg-[var(--neon--green-soft)] text-[var(--neon-green)] shadow-[0_0_20px_rgba(163, 255, 18, 0.12)]` : `bg-transparent text-zinc-500 hover:text-white`}`}
+        className={`rounded-full px-6 py-3 text-[20px] tracking-wide font-semibold transition-all duration-300 md:px-8 md:py-4 cursor-pointer ${activeTab == "locations" ? `bg-[var(--neon--green-soft)] text-[var(--neon-green)] shadow-[0_0_20px_rgba(163, 255, 18, 0.12)]` : `bg-transparent text-zinc-500 hover:text-white`}`}
         style={{ fontFamily: "Orbitron, sans-serif" }}
       >
         Locations
