5757dc49d4be34f1fef6b7afd59df20d9de8801d
monicaortiz20 <ortiznavajomonica@gmail.com>
2026-05-24 00:38:12 +0200

    styling pagination Component

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -7,22 +7,69 @@ export default function Pagination({
   onNext,
 }: PaginationProps) {
   return (
-    <div className="flex items-center gap-4 mt-8">
+    <div
+      className="flex flex-col
+    items-center
+    justify-cennter
+    gap-5
+    py-6
+    md:flex-row"
+    >
       <button
         onClick={onPrev}
         disabled={currPage === 1}
-        className="px-4 py-2 border rounded disabled:opacity-50"
+        className="flex h-[56px] w-[56px]
+        items-center
+        justify-center
+        rounded-2x1 border
+        border-white/5
+        bg-white/[0.03]
+        text-2x1
+        text-white
+        transition-all
+        duration-300
+        cursor-pointer
+        hover:border-[rgba(163, 255, 18, 0.2)] hover:text-[var(--neon-green)]
+        hover:shadow-[0_0_20px_rgba(163, 255, 18, 0.08)]
+        disabled:cursor-not-allowed
+        disabled:opacity-30
+        disabled:hover:border-white/5
+        disabled:hover:text-white
+        disabled:hover:shadow-none"
       >
         &#x2190;
       </button>
-      <span>
-        {" "}
-        Page {currPage} of {totalPages}
+
+      <span
+        className="text-sm uppercase tracking-[0.18em]
+        text-zinc-400
+        md:text-base"
+        style={{ fontFamily: "Orbitron, sans-serif" }}
+      >
+        Page <span className="text-[vaar(--neon-green)]">{currPage}</span> of{" "}
+        {totalPages}
       </span>
       <button
         onClick={onNext}
         disabled={currPage === totalPages}
-        className="px-4 py-2 border rounded disabled:opacity-50"
+        className="flex h-[56px] w-[56px]
+        items-center
+        justify-center
+        rounded-2x1 border
+        border-white/5
+        bg-white/[0.03]
+        text-2x1
+        text-white
+        transition-all
+        duration-300
+        cursor-pointer
+        hover:border-[rgba(163, 255, 18, 0.2)] hover:text-[var(--neon-green)]
+        hover:shadow-[0_0_20px_rgba(163, 255, 18, 0.08)]
+        disabled:cursor-not-allowed
+        disabled:opacity-30
+        disabled:hover:border-white/5
+        disabled:hover:text-white
+        disabled:hover:shadow-none"
       >
         &#x2192;
       </button>
